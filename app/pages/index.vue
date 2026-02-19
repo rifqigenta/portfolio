78742aabdb1f48721e296192d7edbfd5410711fc
rifqigenta <rifqigentabuana@students.amikom.ac.id>
2026-02-20 04:54:49 +0700

    kurang email section + responsiveness

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -768,18 +768,13 @@ onUnmounted(() => {
       <SplashCursor
         :SIM_RESOLUTION="128"
         :DYE_RESOLUTION="1440"
-        :CAPTURE_RESOLUTION="512"
         :DENSITY_DISSIPATION="3.5"
         :VELOCITY_DISSIPATION="2"
         :PRESSURE="0.7"
-        :PRESSURE_ITERATIONS="20"
         :CURL="30"
         :SPLAT_RADIUS="0.2"
         :SPLAT_FORCE="9000"
-        :SHADING="true"
         :COLOR_UPDATE_SPEED="10"
-        :BACK_COLOR="{ r: 0.5, g: 0, b: 0 }"
-        :TRANSPARENT="true"
       />
     </ClientOnly>
     <section ref="heroSection" class="hero-section relative min-h-screen flex items-center px-10 lg:px-24 overflow-hidden">
